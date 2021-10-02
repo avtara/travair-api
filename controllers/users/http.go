e6778f885e4ed61b85bc224d6375634cba0bd4a7
avtar <avtarakhrisna1@gmail.com>
2021-10-02 15:31:21 +0700

    fix: add return when find error

--- controllers/users/http.go
+++ controllers/users/http.go
@@ -29,7 +29,7 @@ func (ctrl *UserController) Registration(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, helpers.BuildErrorResponse("Internal Server Error", err.Error(), helpers.EmptyObj{}))
 	}
 	if err := c.Validate(req); err != nil {
-		c.JSON(http.StatusBadRequest, helpers.BuildErrorResponse("An error occurred while validating the request data", err.Error(), helpers.EmptyObj{}))
+		return c.JSON(http.StatusBadRequest, helpers.BuildErrorResponse("An error occurred while validating the request data", err.Error(), helpers.EmptyObj{}))
 	}
 
 	res,err := ctrl.userService.Registration(ctx, req.ToDomain())
