9c0266fa0819b092c5177a39df7ccaf4fcf66b1d
avtar <avtarakhrisna1@gmail.com>
2021-10-04 20:10:31 +0700

    feat: repo ipapi

---
+++ repository/thirdparties/ipapi/apiRequest.go
@@ -0,0 +1,37 @@
+package ipapi
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/avtara/travair-api/businesses/iplocator"
+	"net/http"
+)
+
+type IpAPI struct {
+	httpClient http.Client
+}
+
+func NewIpAPI() iplocator.Repository {
+	return &IpAPI{
+		httpClient: http.Client{},
+	}
+}
+
+func (ipl *IpAPI) GetLocationByIP(ctx context.Context, ip string) (*iplocator.Domain, error) {
+	req, _ := http.NewRequest("GET", "https://ipapi.co/"+ip+"/json/", nil)
+	req.Header.Set("User-Agent", "ipapi.co/#go-v1.3")
+	resp, err := ipl.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	data := Response{}
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data.toDomain(), nil
+}
